ede266a5f474fdaf63e750a95325e067f7ef7ea0
Aayush Work <aayush@catalan.ai>
2023-09-23 18:01:31 -0400

    push fix resume

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,7 @@ const Nav = ({ isHome }) => {
   // );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="/v5/resume.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
